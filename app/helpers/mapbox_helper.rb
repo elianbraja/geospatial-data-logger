469f2fe68cf236917dc6d7c6fddfad87fa98b8fb
Elian Braja <elian@motomtech.com>
2023-11-08 18:15:57 +0100

    Configure Rails credentials

--- app/helpers/mapbox_helper.rb
+++ app/helpers/mapbox_helper.rb
@@ -4,7 +4,7 @@ module MapboxHelper
   def mapbox_image_url(well_known_text)
     coordinates = format_coordinates(well_known_text)
     url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson({\"type\":\"FeatureCollection\",\"features\":[{\"id\":\"4c97769717bde5d3ece6aa37ad153a26\",\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[#{coordinates}]}," \
-      '"properties":{"title":""}}]})/auto/1200x1200?access_token=pk.eyJ1IjoiZWxpYW5icmFqYSIsImEiOiJjbG9va3B6eHMwMTJxMmlsOTNrdHFnaHl4In0.xcGyUJMs8EqpDiQbbW873g'
+      "\"properties\":{\"title\":\"\"}}]})/auto/1200x1200?access_token=#{Rails.application.credentials.mapbox_key}"
     image_tag(url)
   end
 
